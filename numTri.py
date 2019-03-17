6847659e10da7f8781eb5e17b66019cb5d90b8fa
knakatani29 <knakatani@haverford.edu>
2019-03-17 21:18:06 +0000

    Modifying

--- numTri.py
+++ numTri.py
@@ -1,2 +1,22 @@
 # this is the function to count the number of triangulations
 # for a convex polygon
+def numTri(n):
+    res == []
+    if len(n) == 3:
+        return 
+    else:
+        for i in range(3, len(n)):
+            x = []
+            y = []
+            for a in range(i):
+                x += [n[a]]
+            for b in range(i-1, len(n)):
+                y += [n[b]]
+            res += [[numTri(x), (n[0], n[i]), numTri(y)]]
+        z = []
+        for c in range(1, len(n)):
+            z += [n[c]]
+        res += [[numTri(z), (n[1], n[len(n)-1])]]
+    print(len(res))
+    return res
+        
